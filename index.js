d0d5404a78ac63904da6119dec25d9022b219a90
oscarnovember <ndukwe.peter@roqqu.com>
2023-09-14 00:31:10 +0100

    connect to mongoose db

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const personRouter = require('./person/router');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -7,6 +9,10 @@ app.use(express.json());
 
 app.use('/api', personRouter);
 
-app.listen(PORT, function() {
-  console.log('server listening on port ' + PORT);
+mongoose.connect(process.env.DB_URL).then(function() {
+  app.listen(PORT, function() {
+    console.log('server listening on port ' + PORT);
+  });
+}).catch(function(err) {
+  console.log('failed to connect to db');
 })
